a8a36940cd8578b7efa68b2ee7fa29e8eec30554
望海潮 <yinqunbo@126.com>
2022-01-02 20:00:44 +0800

    :tada: init

---
+++ scripts/deploy.js
@@ -0,0 +1,28 @@
+/*
+ * @Author: 望海潮
+ * @Date: 2021-12-29 20:46:27
+ * @LastEditTime: 2022-01-02 19:07:14
+ * @Description: 
+ */
+const main = async () => {
+  const waveContractFactory = await hre.ethers.getContractFactory("WavePortal");
+  const waveContract = await waveContractFactory.deploy({
+    value: hre.ethers.utils.parseEther("0.001"),
+  });
+
+  await waveContract.deployed();
+
+  console.log("WavePortal address: ", waveContract.address);
+};
+
+const runMain = async () => {
+  try {
+    await main();
+    process.exit(0);
+  } catch (error) {
+    console.error(error);
+    process.exit(1);
+  }
+};
+
+runMain();
